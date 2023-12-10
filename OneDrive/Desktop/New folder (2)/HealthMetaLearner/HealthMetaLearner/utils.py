d4edd5601b0e6425bcb269007a2a85f5594cb31c
rahul-krdas <107635579+rahul-krdas@users.noreply.github.com>
2023-12-11 01:20:47 +0530

    Update utils.py

--- OneDrive/Desktop/New folder (2)/HealthMetaLearner/HealthMetaLearner/utils.py
+++ OneDrive/Desktop/New folder (2)/HealthMetaLearner/HealthMetaLearner/utils.py
@@ -1,4 +1,27 @@
-# utils.py
+# health_meta_learner/utils.py
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+def preprocess_data(data):
+    """
+    Preprocess the input data using standard scaling.
+
+    Parameters:
+    - data: Input data to be preprocessed.
+
+    Returns:
+    - Preprocessed data.
+    """
+    scaler = StandardScaler()
+    preprocessed_data = scaler.fit_transform(data)
+    return preprocessed_data
 
 def helper_function():
-    print("Hello from Utils!")
+    """
+    A placeholder for a helper function that might be needed in the package.
+
+    Parameters: None
+
+    Returns: None
+    """
+    print("This is a placeholder for a helpful utility function.")
